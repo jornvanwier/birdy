7fa8d8d17523eb2553b4ecaef4d0cf526c6bf593
Jorn van Wier <mail@jornvanwier.com>
2026-08-12 13:28:42 +0200

    Map all inputs

--- src/input.rs
+++ src/input.rs
@@ -13,6 +13,26 @@ pub enum Action {
 
 pub fn create_input_map() -> InputMap<Action> {
     InputMap::<Action>::default()
-        .with_axis(Action::Throttle, VirtualAxis::new(GamepadButton::DPadDown, GamepadButton::DPadUp))
-        .with_axis(Action::Throttle, VirtualAxis::new(KeyCode::ControlLeft, KeyCode::ShiftLeft))
+        // Throttle: Shift (Up) / Ctrl (Down) or DPad Up/Down
+        .with_axis(
+            Action::Throttle,
+            VirtualAxis::new(KeyCode::ControlLeft, KeyCode::ShiftLeft),
+        )
+        .with_axis(
+            Action::Throttle,
+            VirtualAxis::new(GamepadButton::DPadDown, GamepadButton::DPadUp),
+        )
+        // Roll & Pitch: WASD, Arrow Keys, or Left Stick
+        .with_dual_axis(Action::RollPitch, VirtualDPad::wasd())
+        .with_dual_axis(Action::RollPitch, VirtualDPad::arrow_keys())
+        .with_dual_axis(Action::RollPitch, GamepadStick::LEFT)
+        // Yaw: Q (Left) / E (Right) or Gamepad Triggers
+        .with_axis(
+            Action::Yaw,
+            VirtualAxis::new(KeyCode::KeyQ, KeyCode::KeyE),
+        )
+        .with_axis(
+            Action::Yaw,
+            VirtualAxis::new(GamepadButton::LeftTrigger2, GamepadButton::RightTrigger2),
+        )
 }
